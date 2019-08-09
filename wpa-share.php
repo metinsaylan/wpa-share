c6b5b571220d1381c2f8ad191e5fe3bf2c370e9c
Metin Şaylan <metinsaylan@gmail.com>
2019-08-10 00:52:13 +0300

    enable auto insert options

--- wpa-share.php
+++ wpa-share.php
@@ -59,6 +59,7 @@ if( ! function_exists( 'wpa_share' ) ){
         )
     );
 
+    /* Thin wrap for wpa_plugin */
     function get_wpa_share_option( $key, $default ){
         global $wpa_share;
         return $wpa_share->get_setting( $key, $default );
@@ -66,9 +67,14 @@ if( ! function_exists( 'wpa_share' ) ){
 
     add_action( 'wp_head', 'wpa_share_print_styles' );
     function wpa_share_print_styles(){
+
+        $output_css = get_wpa_share_option( 'output-css', 'on' );
+
+        if( 'on' === $output_css ){ 
         ?>
 <style>.sr-only{position:absolute;width:1px;height:1px;padding:0;overflow:hidden;clip:rect(0,0,0,0);white-space:nowrap;border:0}.wpa-share{display:block;padding:8px;margin:0 auto 15px;text-align:center}.wpa-share-label{display:inline-block; margin-right: 20px}.wpa-share-btn{display:inline-block;background:rgba(255,255,255,0.8);color:#777;text-decoration:none;border-radius:50%;padding:5px 8px;margin-right:20px;box-shadow: rgba(0,0,0,0.2) 0 2px 5px;}.wpa-share-btn svg{margin:0;padding:0;vertical-align:middle}</style>
         <?php
+        }
     }
 
     function wpa_share(){
@@ -108,6 +114,8 @@ if( ! function_exists( 'wpa_share' ) ){
         $share_buttons .= '<a class="wpa-share-btn" target="_blank" title="Tweet this post" href="http://twitter.com/share?url='. $link . '&text=' . $desc . '"><svg xmlns="http://www.w3.org/2000/svg" width="' . $button_size . '" height="' . $button_size . '" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg> <span class="sr-only">'. __( 'Tweet', 'wpa-share' ) . '</span></a>';
 
         $share_buttons .= '<a  class="wpa-share-btn" target="_blank" title="Pin on Pinterest" href="http://pinterest.com/pin/create/button/?url='. $link . '&media='. $img_src . '&description='. $desc . '" ><svg xmlns="http://www.w3.org/2000/svg" width="' . $button_size . '" height="' . $button_size . '" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146 1.124.347 2.317.535 3.554.535 6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z" fill-rule="evenodd" clip-rule="evenodd"/></svg> <span class="sr-only">'. __( 'Pin', 'wpa-share' ) . '</span></a>';
+
+        $share_buttons .= '</div>';
         
         return $share_buttons;
     }
@@ -123,8 +131,30 @@ if( ! function_exists( 'wpa_share' ) ){
         return $links;
     }
     
-    
-    
-    
+    add_filter( 'the_content', 'wpa_share_auto_insert_filter' );
+    function wpa_share_auto_insert_filter( $content ) {
+        global $post;
 
+        /* Check auto-insert option */
+        $wpa_auto_insert = get_wpa_share_option( 'auto-insert', 'off' );
+        if( 'off' === $wpa_auto_insert ) {
+            return $content;
+        }
+
+        $auto_insert_location = get_wpa_share_option( 'auto-insert-location', '' );
+
+        $auto_insert_label = '';
+        $auto_insert_button_size = '42';
+        $share_buttons_html = wpa_share_get_buttons( $auto_insert_label, $auto_insert_button_size );
+
+        if( 'before-post' === $auto_insert_location ){
+            return $share_buttons_html . $content;
+        } elseif ( 'after-post' === $auto_insert_location ){
+            return $content . $share_buttons_html;
+        } else {
+            return $share_buttons_html . $content . $share_buttons_html;
+        }
+
+    }
+    
 }
