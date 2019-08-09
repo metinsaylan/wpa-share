0f657c5fb447e7040bf8d8eda14f162c385d4330
Metin Şaylan <metinsaylan@gmail.com>
2019-08-09 23:58:51 +0300

    Create wpa-sidebar.php

---
+++ inc/wpa-sidebar.php
@@ -0,0 +1,21 @@
+<div class="card primary">
+Do you like <strong>WPA Share</strong>? You have many ways to show your appreciation:
+<ul>
+    <li>⭐ Rate it on <a href="https://wpassist.me/plugins/wpa-share/" target="_blank" >our website</a></li>
+    <li>☕ <a href="https://wpassist.me/donate/" target="_blank" >Donate</a> using Stripe</li>
+    <li>📢 <a href="https://wpassist.me/spread/wpa-share-plugin/" target="_blank" >Spread the word</a></li>
+</ul>
+
+If you have any suggestions, or bug reports, please <a href="https://wpassist.me/plugins/wpa-share/">write to us</a> on the <a href="https://wpassist.me/plugins/wpa-share/">plugin page</a>.
+</div>
+
+<div class="card">
+Check out our other plugins:
+<ul>
+    <li>⏳ <a href="https://metinsaylan.com/wordpress/plugins/countdown/" target="_blank" >Countdown Widget</a></li>
+    <li>💰 <a href="https://metinsaylan.com/wordpress/plugins/adsense-widget/" target="_blank" >Adsense Widget</li>
+    <li>🌳 <a href="https://metinsaylan.com/wordpress/plugins/subpages-widget/" target="_blank" >Subpages Extended</a></li>
+</ul>
+
+If you have any suggestions, or bug reports, please <a href="https://wpassist.me/plugins/wpa-share/">write to us</a> on the <a href="https://wpassist.me/plugins/wpa-share/">plugin page</a>.
+</div>
